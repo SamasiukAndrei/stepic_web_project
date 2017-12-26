24060d49a7e5e85f39510efdcd8cdbf7edbc76e5
SamasiukAndrei <a.samosuk@gmail.com>
2017-12-26 12:30:10 +0300

    Edit configs.

--- etc/hello.py
+++ etc/hello.py
@@ -1,1 +1,11 @@
-def wsgi_application(environ, start_response):
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web/hello.py',
+    #'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=16',
+        '--timeout=60',
+        'hello:app',
+    ),
+}
