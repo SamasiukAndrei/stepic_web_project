dda2514983a1004f14739eece7d61903b190ed70
SamasiukAndrei <a.samosuk@gmail.com>
2017-12-26 14:20:24 +0300

    All done!

--- hello.py
+++ hello.py
@@ -1,13 +1,4 @@
 def app(environ, start_response):
-    start_response("200 OK", [
-        ("Content-Type", "text/plain")
-    ])
-    #tart_response('200 OK', [('Content-Type', 'text/plain')])
-    #qs = parse_qs(environ['QUERY_STRING'])
-    #return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
-    #body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    #return body
-
     body = '\n'.join(environ['QUERY_STRING'].split('&'))
-    start_response(status, headers)
+    start_response('200 OK', [('Content-Type', 'text/plain')])
     return [body]
--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+sudo rm -f /etc/nginx/conf.d/default.conf
+sudo rm -f /etc/nginx/sites-avaliable/default
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/conf.d/test.conf
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
