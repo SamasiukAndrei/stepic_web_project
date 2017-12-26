4ea2f43932cd1c244bc6c1394f4045d9d4151485
SamasiukAndrei <a.samosuk@gmail.com>
2017-12-26 13:09:09 +0300

    Edit hello.py.

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
+    start_response("200 OK", [
+        ("Content-Type", "text/plain")
+    ])
+    #tart_response('200 OK', [('Content-Type', 'text/plain')])
     #qs = parse_qs(environ['QUERY_STRING'])
     #return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
